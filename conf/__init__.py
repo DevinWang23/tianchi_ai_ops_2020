57e752912d6742fdef1150e8e5c3b9657adbbcd2
root <wangmengqiu@ainnovation.com>
2020-02-19 13:13:58 +0800

    add conf and jupyter folders

---
+++ conf/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+CODE_VERSION = "v1.0 alpha released at 2020.01"
+ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+LOG_DIR = os.path.join(ROOT_DIR, 'log')
+LIB_DIR = os.path.join(ROOT_DIR, 'lib')
