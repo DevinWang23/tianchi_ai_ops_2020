fc101337811f7bcfcedcfd1ebc4c09c0eaf61814
wangmengqiu <wangmengqiu@ainnovation.com>
2020-02-19 12:50:41 +0800

    add log and test folders

---
+++ tests/test_feature_engineering.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Author: MengQiu Wang 
+Email: wangmengqiu@ainnovation.com
+Date: 29/10/2019
+
+Description:
+    Test feature engineering
+"""
+import sys
+
+
+def test_feature_engineering():
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    pass
